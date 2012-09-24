2f54cd14c5931b9bfbb89a5410699457d105e6d2
Erik Steigler <erik@lot18.com>
2012-09-24 16:48:22 -0400

    Update the startup script for the pystatsd stuff too

--- recipes/statsd.rb
+++ recipes/statsd.rb
@@ -15,5 +15,7 @@ cookbook_file "/etc/default/pystatsd" do
 end
 
 service "pystatsd" do
-  action :start
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
 end
